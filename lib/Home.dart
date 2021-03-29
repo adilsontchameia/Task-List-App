f10ee1e3634fbf2e0a395384434cbf6920405fb8
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-29 16:31:39 +0100

    criacao da listView

--- lib/Home.dart
+++ lib/Home.dart
@@ -6,23 +6,41 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  //Iniciado lista vazia
+  List _tarefas = [];
+  //Carregar a lista na View
+  void _carregarTarefas() {
+    _tarefas = [];
+    //Criado um laco de repeticao para encher a lista na view
+    for (int i = 0; i <= 2; i++) {
+      //Criacao de um Map de strings e dynamics
+      Map<String, dynamic> itens = Map();
+      itens["titulo"] = "Adilson $i";
+      //Adicionando Item a lista
+      _tarefas.add(itens);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    //Iniciando a lista a iniciar o app
+    _carregarTarefas();
     return Scaffold(
       //AppBar
       appBar: AppBar(
         title: Text("Task App"),
         backgroundColor: Colors.orange,
       ),
-      body: Container(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print("Pressionado");
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.orange,
-        //elevation: 6, - Valor padrao da sombra
-        //mini: true, - Tamanho mini
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: ListView.builder(
+            itemBuilder: (context, indice) {
+              //Retorna numa listTitle
+              return ListTile(
+                title: Text(_tarefas[indice]["titulo"]),
+              );
+            },
+            itemCount: _tarefas.length),
       ),
     );
   }
